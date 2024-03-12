e961b855cad9290911d0599c75ff0597fc367268
Fábio Alarcon de Almeida <124197923+Roolligan@users.noreply.github.com>
2024-03-12 20:11:52 -0300

    fase01

---
+++ main.js
@@ -0,0 +1,44 @@
+function tocaSomPom () {
+    document.querySelector('#som_tecla_pom').play();
+}
+document.querySelector('.tecla_pom').onclick = tocaSomPom;
+
+function tocaSomClap () {
+    document.querySelector('#som_tecla_clap').play();
+}
+document.querySelector('.tecla_clap').onclick = tocaSomClap;
+
+
+function tocaSomTim () {
+    document.querySelector('#som_tecla_tim').play();
+}
+document.querySelector('.tecla_tim').onclick = tocaSomTim;
+
+function tocaSomPuff () {
+    document.querySelector('#som_tecla_puff').play();
+}
+
+function tocaSomSplash () {
+    document.querySelector('#som_tecla_splash').play();
+}
+function tocaSomToim () {
+    document.querySelector('#som_tecla_toim').play();
+}
+
+function tocaSomPsh () {
+    document.querySelector('#som_tecla_psh').play();
+}
+
+function tocaSomTic () {
+    document.querySelector('#som_tecla_tic').pay();
+}
+
+function tocaSomTom () {
+    document.querySelector('#som_tecla_tom').pay();
+}
+
+
+
+
+
+
